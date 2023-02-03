2c48f43502d935846de2fc1663f35969aca52876
kimgyuha <kimgyuha25@gmail.com>
2023-02-03 14:48:28 +0900

    📝:: router 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainhome from "./pages/Mainhome";
+import Signup from "./pages/Signup";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={<Mainhome />} />
+        <Route path="/" element={<Mainhome />} />
+        <Route path="signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
