ff488a00de7fdb5c8325ec0426acb752c38da178
kimgyuha <kimgyuha25@gmail.com>
2023-02-18 21:13:05 +0900

    💩 :: 디자인 변경으로 똥산 코드 됨

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainhome from "./pages/Mainhome";
 import Signup from "./pages/AuthPage/signup";
 import Login from "./pages/AuthPage/login";
-import ScheduleHome from "./pages/Schedule";
 
 function App(): JSX.Element {
   return (
@@ -11,7 +10,6 @@ function App(): JSX.Element {
         <Route path="/" element={<Mainhome />} />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
-        <Route path="schedule" element={<ScheduleHome />} />
       </Routes>
     </BrowserRouter>
   );
